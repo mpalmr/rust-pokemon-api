dc473f61a276ffa8d7474cf4b808d83710dbba7a
Matthew Palmer <matt@silvercreekit.com>
2019-04-28 06:13:50 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -6,61 +6,61 @@ use std::io::{self, Write};
 
 #[derive(Deserialize)]
 struct PokemonAbility {
-	name: String,
-	url: String,
+    name: String,
+    url: String,
 }
 
 #[derive(Deserialize)]
 struct PokemonAbilityWrapper {
-	ability: PokemonAbility,
-	is_hidden: bool,
-	slot: u32,
+    ability: PokemonAbility,
+    is_hidden: bool,
+    slot: u32,
 }
 
 #[derive(Deserialize)]
 struct Pokemon {
-	id: u32,
-	name: String,
-	weight: u32,
-	height: u32,
-	abilities: Vec<PokemonAbilityWrapper>,
+    id: u32,
+    name: String,
+    weight: u32,
+    height: u32,
+    abilities: Vec<PokemonAbilityWrapper>,
 }
 
 impl Pokemon {
-	pub fn fetch(name: &String) -> Result<Pokemon, reqwest::Error> {
-		Ok(reqwest::Client::new()
-			.get(&format!("https://pokeapi.co/api/v2/pokemon/{}", name))
-			.send()?
-			.json()?)
-	}
+    pub fn fetch(name: &String) -> Result<Pokemon, reqwest::Error> {
+        Ok(reqwest::Client::new()
+            .get(&format!("https://pokeapi.co/api/v2/pokemon/{}", name))
+            .send()?
+            .json()?)
+    }
 }
 
 fn main() {
-	let query = prompt_name().expect("Could not retrieve user input");
-	let pokemon = Pokemon::fetch(&query).expect("Could not retrieve pokemon");
+    let query = prompt_name().expect("Could not retrieve user input");
+    let pokemon = Pokemon::fetch(&query).expect("Could not retrieve pokemon");
 
-	println!("\n\nBasic Info\n==========");
-	println!(
-		"id: {id}\nname: {name}\nweight: {weight}\nheight: {height}\n",
-		id = pokemon.id,
-		name = pokemon.name,
-		weight = pokemon.weight,
-		height = pokemon.height,
-	);
-	println!("\nAbilities\n=========");
-	for ability_wrapper in pokemon.abilities.iter() {
-		println!(
-			"{slot}. {name}",
-			slot = ability_wrapper.slot,
-			name = ability_wrapper.ability.name,
-		);
-	}
+    println!("\n\nBasic Info\n==========");
+    println!(
+        "id: {id}\nname: {name}\nweight: {weight}\nheight: {height}\n",
+        id = pokemon.id,
+        name = pokemon.name,
+        weight = pokemon.weight,
+        height = pokemon.height,
+    );
+    println!("\nAbilities\n=========");
+    for ability_wrapper in pokemon.abilities.iter() {
+        println!(
+            "{slot}. {name}",
+            slot = ability_wrapper.slot,
+            name = ability_wrapper.ability.name,
+        );
+    }
 }
 
 fn prompt_name() -> Result<String, Box<std::error::Error>> {
-	let mut input = String::new();
-	print!("Name a pokemon: ");
-	io::stdout().flush()?;
-	io::stdin().read_line(&mut input)?;
-	Ok(input.trim().parse()?)
+    let mut input = String::new();
+    print!("Name a pokemon: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut input)?;
+    Ok(input.trim().parse()?)
 }
