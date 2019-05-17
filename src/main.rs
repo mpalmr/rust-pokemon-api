4dd3af47de3812a8a34bf09c7ce40c460ecb878b
Matthew Palmer <matt@silvercreekit.com>
2019-05-17 18:22:04 -0400

    reduce number of files

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,106 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
 
-mod api;
 mod pokemon;
-mod prompt;
 
-use crate::pokemon::Pokemon;
-use prompt::MoreDetailsOption;
+use crate::pokemon::{Ability, Pokemon};
+use colored::*;
+use std::io::{self, Write};
 
 fn main() {
     loop {
-        let pokemon_name = &prompt::name();
+        let pokemon_name = &name_prompt();
         if pokemon_name == "q" {
             break;
         }
 
         if let Ok(pokemon) = Pokemon::new(pokemon_name) {
             println!("\n{}", pokemon);
-            match prompt::more_details() {
+            match more_details_prompt() {
                 MoreDetailsOption::Exit => break,
                 MoreDetailsOption::SearchPokemon => println!("\n"),
                 MoreDetailsOption::Abilities => {
-                    if let Some(ability) = prompt::ability(&pokemon) {
+                    if let Some(ability) = ability_prompt(&pokemon) {
                         println!("\n\n{}", ability);
                     }
                 }
             }
         }
+    }
+}
+
+fn name_prompt() -> String {
+    let mut input = String::new();
+    print!("{} ", "Name a pokemon or \"q\" to quit:".green().bold());
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<String>().unwrap()
+}
+
+enum MoreDetailsOption {
+    Exit,
+    Abilities,
+    SearchPokemon,
+}
+
+fn more_details_prompt() -> MoreDetailsOption {
+    println!(
+        "\n{}",
+        "What would you like to know more about?".green().bold()
+    );
+    println!("{} {}", "[1]".blue().bold(), "Abilities".blue());
+    println!(
+        "{} {}",
+        "[2]".blue().bold(),
+        "Search for new pokemon".blue()
+    );
+    println!("{} {}\n", "[0]".red().bold(), "Exit".red());
+
+    loop {
+        let mut input = String::new();
+        print!("Option: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        match input.trim().parse::<u8>().unwrap() {
+            0 => return MoreDetailsOption::Exit,
+            1 => return MoreDetailsOption::Abilities,
+            2 => return MoreDetailsOption::SearchPokemon,
+            _ => println!("{}", "Invalid option.".red()),
+        }
+    }
+}
+
+fn ability_prompt(pokemon: &Pokemon) -> Option<&Ability> {
+    loop {
+        println!(
+            "\n{}",
+            "Pick one from the following abilities:".green().bold()
+        );
+        pokemon
+            .abilities
+            .iter()
+            .enumerate()
+            .for_each(|(i, ability)| {
+                println!(
+                    "{} {}",
+                    &format!("[{}]", (i + 1).to_string()).blue().bold(),
+                    ability.name.blue(),
+                )
+            });
+        println!("{} {}\n", "[0]".red().bold(), "Go Back".red());
+
+        let mut input = String::new();
+        print!("Option: ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim().parse::<usize>().unwrap();
+
+        if input == 0 {
+            return None;
+        }
+        if let Some(ability) = pokemon.abilities.get(input - 1) {
+            return Some(&ability);
+        }
+        println!("{}", "Invalid option.".red());
     }
 }
--- src/pokemon.rs
+++ src/pokemon.rs
@@ -1,4 +1,3 @@
-use crate::api;
 use std::fmt;
 
 pub struct Ability {
@@ -50,5 +49,44 @@ impl fmt::Display for Pokemon {
         self.abilities
             .iter()
             .try_for_each(|ability| writeln!(f, "{}", ability.name))
+    }
+}
+
+mod api {
+    use reqwest::{Client, Error};
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    pub struct Pokemon {
+        pub id: u32,
+        pub name: String,
+        pub weight: u32,
+        pub height: u32,
+        pub abilities: Vec<AbilityWrapper>,
+    }
+
+    #[derive(Deserialize)]
+    pub struct Ability {
+        pub name: String,
+        pub url: String,
+    }
+
+    #[derive(Deserialize)]
+    pub struct AbilityWrapper {
+        pub ability: Ability,
+        pub is_hidden: bool,
+    }
+
+    impl Pokemon {
+        pub fn get_by_name(name: &str) -> Result<Self, Error> {
+            Ok(Self::fetch(&format!("pokemon/{}", name))?)
+        }
+
+        fn fetch(url: &str) -> Result<Self, Error> {
+            Ok(Client::new()
+                .get(&format!("https://pokeapi.co/api/v2/{}", url))
+                .send()?
+                .json()?)
+        }
     }
 }
